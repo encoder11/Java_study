2b9e41535946140ff7105affe7714f56d2f76142
KTH <KTH@DESKTOP-3GOFLG1>
2020-07-09 22:59:26 +0900

    test

--- z_java/src/com/naver/test/TestClass.java
+++ z_java/src/com/naver/test/TestClass.java
@@ -2,4 +2,50 @@ package com.naver.test;
 
 public class TestClass {
 	
+	
+	
 }
+/*
+#include<iostream>
+using namespace std;
+
+int i, temp;
+int reversearray(int xxx[], int size);
+
+int main() {
+   int A[10] = { 1,2,3,4,5,6,7,8,9,10 };
+   int B[15] = { 3,2,10,1,8,15,6,17,11,19,10,4,5,21,22 };
+   
+   cout << "-------------------------\n";
+   cout << ": 학번: 2014219045      :\n";
+   cout << ": 이름: 신명진          :\n";
+   cout << "-------------------------\n";
+   cout << endl;
+
+
+	reversearray(A, 9);
+	
+}
+
+int reversearray(int xxx[], int size){
+	int printSize = size;
+	int cnt = 0;
+	   while (1) {
+	      if (cnt >= size) break;
+	      temp = xxx[cnt];
+	      xxx[cnt] = xxx[size];
+	      
+	      xxx[size] = temp;
+	      cnt++;
+	      size--;
+	   }
+	for (i = 0; i < 10; i++) cout << xxx[i] << " ";
+	   return xxx;
+}
+
+void printArray(){
+	
+	for(i=0 ; i<size ; i++) cout << yyy[i] << " ";
+	
+}
+*/
